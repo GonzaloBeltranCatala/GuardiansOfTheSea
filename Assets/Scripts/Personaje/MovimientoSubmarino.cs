1c2def983e55e78a1c7871e51746786315d43b09
SantiagoAmores <151826439+SantiagoAmores@users.noreply.github.com>
2024-01-12 10:07:48 +0100

    SUBMARINO
    Sprite temporal y script de movimiento básico

--- Assets/Scripts/Personaje/MovimientoSubmarino.cs
+++ Assets/Scripts/Personaje/MovimientoSubmarino.cs
@@ -7,12 +7,21 @@ public class MovimientoSubmarino : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    public float velocidad;
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+
+        if (inputVector.magnitude > 1) { inputVector.Normalize(); }
+
+        Vector3 movementVector = inputVector * velocidad * Time.deltaTime;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x + movementVector.x, -9, 9), Mathf.Clamp(transform.position.y + movementVector.y, -4, 4), 0);
+
     }
 }
