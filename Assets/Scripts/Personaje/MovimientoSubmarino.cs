bec9b2725539f677cea7242e81b450ba32f3818b
GonzaloBeltranCatala <gbeltrancatala@gmail.com>
2024-01-12 13:46:58 +0100

    Movimiento Submarino Arreglos

--- Assets/Scripts/Personaje/MovimientoSubmarino.cs
+++ Assets/Scripts/Personaje/MovimientoSubmarino.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class MovimientoSubmarino : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+
     public float velocidad;
-    // Update is called once per frame
+
+    private SpriteRenderer spriteRenderer;
+
+    public float profundidadMax;
+
+
+    private void Awake()
+    {
+       
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
     void Update()
     {
 
         Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 
 
-        if (inputVector.magnitude > 1) { inputVector.Normalize(); }
+        if (inputVector.magnitude > 1) {
+            
+            inputVector.Normalize(); 
+        
+        }
 
         Vector3 movementVector = inputVector * velocidad * Time.deltaTime;
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x + movementVector.x, -9, 9), Mathf.Clamp(transform.position.y + movementVector.y, -4, 4), 0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x + movementVector.x, -8, 8), Mathf.Clamp(transform.position.y + movementVector.y, profundidadMax, - 2.5f), 0);
+
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            spriteRenderer.flipX = true;
+
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
 
     }
 }
