4aa0e7c8b93a68a7e6ddf31ad2494b2c2e028506
Manuel13400 <manueldelarosa1340@gmail.com>
2024-01-16 19:33:28 +0100

    Paredes, suelo, tiburon desvanecimiento
    Nuevas paredes, fondo temporal añadido, tiburon se desvanece al dispararle

--- Assets/Scripts/Enemigos/MovimientoTiburon.cs
+++ Assets/Scripts/Enemigos/MovimientoTiburon.cs
@@ -7,33 +7,64 @@ public class MovimientoTiburon : MonoBehaviour
     public Transform objetivo;
     public float velocidad = 0.5f;
     public float distanciaMinima = 1f;
+    public float velocidadDesvanecimiento = 0.2f;
+    public float opacidadInicial = 1f;
 
     private Vector3 direccion = Vector3.left;
     private SpriteRenderer spriteRenderer;
 
+    private Animator animator;
+    public Sprite spriteDefault;
+    public Sprite spriteAlternativo;
+
+    private bool estaDesvaneciendose = false;
+
+    private Collider2D tiburonCollider;
+
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        tiburonCollider = GetComponent<Collider2D>();
+        CambiarSprite(spriteDefault);
     }
 
     void Update()
     {
-        Vector3 direccion = objetivo.position - transform.position;
-        direccion.Normalize();
-        transform.position += direccion * velocidad * Time.deltaTime;
+        if (!estaDesvaneciendose)
+        {
+            Vector3 direccion = objetivo.position - transform.position;
+            direccion.Normalize();
+            transform.position += direccion * velocidad * Time.deltaTime;
 
-        spriteRenderer.flipX = (direccion.x > 0);
+            spriteRenderer.flipX = (direccion.x > 0);
+        }
+        else
+        {
+            Color color = spriteRenderer.color;
+            color.a -= velocidadDesvanecimiento * Time.deltaTime;
+            spriteRenderer.color = color;
+
+            if (color.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Submarino") || (collision.CompareTag("Bala")))
+        if (collision.CompareTag("Submarino"))
         {
-            DestruirEnemigo();
+            DestruirTiburon();
+        }
+        else if (collision.CompareTag("Bala"))
+        {
+            ApaciguarTiburon();
         }
     }
 
-    void DestruirEnemigo()
+    void DestruirTiburon()
     {
         AparicionTiburon aparicionEnemigo = FindObjectOfType<AparicionTiburon>();
         if (aparicionEnemigo != null)
@@ -41,5 +72,20 @@ public class MovimientoTiburon : MonoBehaviour
             aparicionEnemigo.AparecerEnemigos();
         }
         Destroy(gameObject);
+    }
+
+    void ApaciguarTiburon()
+    {
+        CambiarSprite(spriteAlternativo);
+        estaDesvaneciendose = true;
+        velocidad *= 0.5f;
+        tiburonCollider.enabled = false;
+    }
+
+    void CambiarSprite(Sprite nuevoSprite)
+    {
+        GetComponent<SpriteRenderer>().sprite = nuevoSprite;
+        spriteRenderer.color = new Color(1f, 1f, 1f, opacidadInicial);
+        animator.Play("Triste", 0, 0f);
     }
 }
