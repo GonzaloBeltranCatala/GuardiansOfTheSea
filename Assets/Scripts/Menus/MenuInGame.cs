403d879122b7a351b690d6a2056401375843d3ba
Miguel6GG <miggargar6@alu.edu.gva.es>
2024-01-16 11:36:48 +0100

    Menu Pausa En Juego
    Ahora el juego tiene un menu pausa in-game y el jugador no está atrapado en un limbo eterno de sufrimiento

---
+++ Assets/Scripts/Menus/MenuInGame.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInGame : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Pausa y continuar
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    //Otros botones
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        isPaused = false;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    //Funcionalidad menu
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Continue();
+            }
+        }
+    }
+}
