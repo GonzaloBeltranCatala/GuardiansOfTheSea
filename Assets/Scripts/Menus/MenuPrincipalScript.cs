7762ba03d9e428f57293750e7a692297685b4969
Miguel6GG <miggargar6@alu.edu.gva.es>
2024-01-18 22:39:37 +0100

    Cinematicas
    Ahora al empezar nueva partida hay cinematicas

--- Assets/Scripts/Menus/MenuPrincipalScript.cs
+++ Assets/Scripts/Menus/MenuPrincipalScript.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipalScript : MonoBehaviour
 {
+    public GameObject Cinematica1;
+    public GameObject Cinematica2;
+    public GameObject Cinematica3;
+
     void Start()
     {
         Scene currentScene = SceneManager.GetActiveScene();
+        Cinematica1.SetActive(false);
+        Cinematica2.SetActive(false);
+        Cinematica3.SetActive(false);
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("GuardiansOfTheSea");
+        StartCoroutine(Sequence());
     }
 
     public void Options()
@@ -28,5 +35,25 @@ public class MenuPrincipalScript : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    private IEnumerator Sequence()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        Cinematica1.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        Cinematica2.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        Cinematica3.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene("GuardiansOfTheSea");
     }
 }
